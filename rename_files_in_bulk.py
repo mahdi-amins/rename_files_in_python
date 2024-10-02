a56c2cea231b1f2617bd7eec2c53af6ef394e012
Mahdi Amins <64124198+mahdi-amins@users.noreply.github.com>
2024-10-02 15:31:31 +0300

    Update rename_files_in_bulk.py
    add comments

--- rename_files_in_bulk.py
+++ rename_files_in_bulk.py
@@ -1,15 +1,15 @@
 from glob import glob
 import os
 
-remove_list = ["sample_site.com - "] 
+remove_list = ["sample_site.com - "] # list of texts you want to remove
 
-files_list = glob("*.mp4")
+files_list = glob("*.mp4") # collect all files that are mp4
 
 for f in files_list:
   for r in remove_list:
     
-    f_new_name = f
+    f_new_name = f # copy the original name
     if r in f_new_name:
-      f_new_name.replace(r,"")
+      f_new_name.replace(r,"") # remove sample_site from your file
       
-    os.rename(f, f_new_name)
+    os.rename(f, f_new_name) # rename the file with new name
